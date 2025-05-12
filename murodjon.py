22913dd09c11cdf7a7a250eac086ef8bee18eb39
cod61 <sirochxontohirxonov5@gmail.com>
2025-05-12 08:08:24 +0300

    yangi kod o'zimcha yozdim

--- murodjon.py
+++ murodjon.py
@@ -1,18 +1,1 @@
-#print("Hello, World!")
-def valyuta_konvertor():
-    print("Valyuta konvertoriga xush kelibsiz!")
-    kurslar = {
-        "USD": 12500,
-        "EUR": 13500
-    }
-
-    miqdor = float(input("So‘m miqdorini kiriting: "))
-    valyuta = input("Qaysi valyutaga o‘tkazmoqchisiz? (USD/EUR): ").upper()
-
-    if valyuta in kurslar:
-        natija = miqdor / kurslar[valyuta]
-        print(f"{miqdor} so‘m = {natija:.2f} {valyuta}")
-    else:
-        print("Noto‘g‘ri valyuta kiritildi!")
-
-valyuta_konvertor()
+print("Hello, World!")
